01f40c08d2b472e4f0a39ccd2fa1865df2098202
marques-j-robinson <marques.j.robinson@gmail.com>
2022-05-22 12:13:32 -0700

    update models urls and views to match the new template layout

--- store/views.py
+++ store/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Category, Product
+from .models import Product
 
 
-def product_all(request):
+def landing_page(request):
+    return render(request, 'home.html')
+
+
+def gallery(request):
     products = Product.products.all()
-    return render(request, 'store/home.html', {'products': products})
+    return render(request, 'gallery.html', {'products': products})
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/single.html', {'product': product})
-
-def category_list(request, category_slug):
-    category = get_object_or_404(Category, slug=category_slug)
-    products = Product.objects.filter(category=category)
-    return render(request, 'store/products/category.html', {'category': category, 'products': products})
+    return render(request, 'product.html', {'product': product})
