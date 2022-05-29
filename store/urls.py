e5bb5028f38fe2619dbaf441d788dfbf794cd520
marques-j-robinson <marques.j.robinson@gmail.com>
2022-05-29 12:10:04 -0700

    update urls to not require trailing forward slash

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,6 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
-    path('gallery/', views.gallery, name='gallery'),
+    path('gallery', views.gallery, name='gallery'),
     path('<slug:slug>', views.product_detail, name='product_detail'),
 ]
